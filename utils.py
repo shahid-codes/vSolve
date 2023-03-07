44e5fa0c71869c69dff470778cc8a7ee8a050657
shahid-codes <80969991+shahid-codes@users.noreply.github.com>
2023-03-07 22:25:19 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 class args:
-    base_model_path = "microsoft/deberta-v3-base"
+    base_model_path = "deberta-v3-base"
     tuned_model_path = "QA-DeBERTa/deberta-v3-base-finetuned.pt"
     max_len = 640
     doc_stride = 128
