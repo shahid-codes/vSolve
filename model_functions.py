faa0ccbc39124b2fabb45bde52441614b88dad1f
shahid-codes <80969991+shahid-codes@users.noreply.github.com>
2023-04-19 06:18:33 +0530

    Update model_functions.py

--- model_functions.py
+++ model_functions.py
@@ -36,7 +36,7 @@ def get_prediction(model,question,context,tokenizer):
     output = model(tokenized_data['input_ids'], tokenized_data['attention_mask'])
     return output.start_logits, output.end_logits, tokenized_data['input_ids']
 
-def postprocess(tokenizer,start_logits,end_logits,input_ids,n_best=20):
+def postprocess(tokenizer,start_logits,end_logits,input_ids,n_best=30,max_answer_length=30):
 
     start_logits = start_logits[0].detach().cpu().numpy()
     start_idxs = np.argsort(start_logits)[-1:-n_best:-1]
@@ -46,13 +46,31 @@ def postprocess(tokenizer,start_logits,end_logits,input_ids,n_best=20):
     score = -np.inf
     start_index = 0
     end_index = 0
+    valid_answers = []
     for i in start_idxs:
         for j in end_idxs:
-            if i <= j and score < start_logits[i]+end_logits[j]:
-                score = start_logits[i]+end_logits[j]
+
+            if j-i+1 > max_answer_length or i>j:
+                continue
+
+            if i <= j:
                 start_index = i
                 end_index = j
-                
-    result = tokenizer.decode(input_ids[0][start_index: end_index+1])          
-    return result, score
 
+                valid_answers.append(
+                    {
+                        'text': tokenizer.decode(input_ids[0][start_index: end_index+1]),
+                        'score': start_logits[i]+end_logits[j]
+                    }
+                )
+                
+    valid_answers = sorted(valid_answers,key = lambda x: x['score'],reverse=True)
+
+    result = valid_answers[0]['text']
+    score = valid_answers[0]['score']
+
+    if score >= 5.0:
+        result = result.title()
+
+    else:
+        result =  "I'm sorry, I don't understand. Can you rephrase the question or ask something else?"
